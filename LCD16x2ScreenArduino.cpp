95be73ad7cb67d0f4bad7944de194eeff24296d4
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2024-02-10 07:31:29 +0100

    EDIT

--- LCD16x2ScreenArduino.cpp
+++ LCD16x2ScreenArduino.cpp
@@ -1,30 +1,27 @@
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
-//Make sure to download Arduino incase you want to see this in action.
+// Compatible with the Arduino IDE 1.0
+// Library version:1.1
+// Make sure to download Arduino incase you want to see this in action.
 
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-int age=20;         
-LiquidCrystal_I2C lcd(0x27,16,2); //Edit this depending on the size of your arduino screen
+int age = 20;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Edit this depending on the size of your arduino screen
 
-void setup(){
-  lcd.init();                      // initialize/start the lcd 
-                                // Print a message to the LCD.
-  lcd.backlight(); 
-  
+void setup()
+{
+  lcd.init(); // initialize/start the lcd
+              // Print a message to the LCD.
+  lcd.backlight();
 }
-
 
 void loop()
 {
-    lcd.setCursor(3,0); //This starts the text 3th colum but on the 1 colum
-    lcd.print("I am learning Arduino, !");
-    lcd.scrollDisplayLeft();
-     lcd.setCursor(2,1);
-    lcd.print("Because I love Robotics");
-    lcd.scrollDisplayLeft();
-    delay(100);
-} 
-
- 
+  lcd.setCursor(3, 0); // This starts the text 3th colum but on the 1 colum
+  lcd.print("I am learning Arduino, !");
+  lcd.scrollDisplayLeft();
+  lcd.setCursor(2, 1);
+  lcd.print("Because I love Robotics");
+  lcd.scrollDisplayLeft();
+  delay(100);
+}
--- LightDetector.cpp
+++ LightDetector.cpp
@@ -1,37 +1,35 @@
-//THIS CODE IS A SMALL SCALE EXAMPLE OF WHAT HAPPENS IN AUTOMATIC STREET LIGHTS
-//YOU CAN USE THIS CODE FOR ANY PROJECTS
+// THIS CODE IS A SMALL SCALE EXAMPLE OF WHAT HAPPENS IN AUTOMATIC STREET LIGHTS
+// YOU CAN USE THIS CODE FOR ANY PROJECTS
 
 int photoResistor = A2;
 int valueOflight;
-int LED=7;
-int LED2=8;
+int LED = 7;
+int LED2 = 8;
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(photoResistor,INPUT);
+  pinMode(photoResistor, INPUT);
   Serial.begin(9600);
-  pinMode(LED,OUTPUT);
-    pinMode(LED2,OUTPUT);
-  
+  pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
 }
 
-void loop() {
-  // pTHIS CODE CONTROLS WHAT HAPPENS WHEN A CERTAIN LIGHT INTENSITY TOUCHES THE PHOTORESISTOR
+void loop()
+{
+  // THIS CODE CONTROLS WHAT HAPPENS WHEN A CERTAIN LIGHT INTENSITY TOUCHES THE PHOTORESISTOR
   valueOflight = analogRead(photoResistor);
   Serial.print("VALUE OF LIGHT IS:  ");
   Serial.println(valueOflight);
-  if(valueOflight>90) //If light intensity is < 90, it's evening/night time and lights are switched on 
-   {
-     digitalWrite(LED, HIGH);
-     digitalWrite(LED2, HIGH);
-  } else 7//Else it's day outside and lights are turned off 
+  if (valueOflight > 90) // If light intensity is < 90, it's evening/night time and lights are switched on
   {
-      digitalWrite(LED,LOW);
-      digitalWrite(LED2,LOW); 
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2, HIGH);
+  }
+  else
+    7 // Else it's day outside and lights are turned off
+    {
+      digitalWrite(LED, LOW);
+      digitalWrite(LED2, LOW);
     }
-    
-
-  
-  
 }

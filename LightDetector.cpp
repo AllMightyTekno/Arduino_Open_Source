a6625a232441a9847ff8349854ca39acea0a722d
AllMightyTekno <143533874+AllMightyTekno@users.noreply.github.com>
2024-02-11 16:45:01 +0100

    Update LightDetector.cpp

--- LightDetector.cpp
+++ LightDetector.cpp
@@ -1,18 +1,17 @@
 // THIS CODE IS A SMALL SCALE EXAMPLE OF WHAT HAPPENS IN AUTOMATIC STREET LIGHTS
 // YOU CAN USE THIS CODE FOR ANY PROJECTS
+//Here's the doc on PhototResistors on the arduino website 
 
 int photoResistor = A2;
 int valueOflight;
-int LED = 7;
-int LED2 = 8;
+int StreetLight = 7;
 
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(photoResistor, INPUT);
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  pinMode(LED2, OUTPUT);
+  pinMode(StreetLight, OUTPUT);
 }
 
 void loop()
@@ -23,13 +22,13 @@ void loop()
   Serial.println(valueOflight);
   if (valueOflight > 90) // If light intensity is < 90, it's evening/night time and lights are switched on
   {
-    digitalWrite(LED, HIGH);
-    digitalWrite(LED2, HIGH);
+    digitalWrite(StreetLight, HIGH);
+    Serial.print(' IT IS NIGHT TIME AND LIGHTS ARE SWITCHED ON ')
   }
   else
     7 // Else it's day outside and lights are turned off
     {
-      digitalWrite(LED, LOW);
-      digitalWrite(LED2, LOW);
+      digitalWrite(StreetLight, LOW);
+      Serial.print(" IT'S DAY OUTSIDE AND LIGHTS ARE SWITCHED OFF ")
     }
 }
